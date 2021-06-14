f560eced5fd3f69e5c86581e43db91102038367f
budokans <68995729+budokans@users.noreply.github.com>
2021-06-15 02:04:11 +1200

    105. A First Class decorator

--- 06-decorators/dist/app.js
+++ 06-decorators/dist/app.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log("Test");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function Logger(constructor) {
+    console.log("Logging");
+    console.log(constructor);
+}
+let Person = class Person {
+    constructor(name = "Steven") {
+        this.name = name;
+        console.log("Create a person object");
+    }
+};
+Person = __decorate([
+    Logger
+], Person);
+const person = new Person();
--- 06-decorators/src/app.ts
+++ 06-decorators/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Test");
+// Note that decorators execute when classes are defined; not when instances of said classes are instantiated.
+
+function Logger(constructor: Function) {
+  console.log("Logging");
+  console.log(constructor);
+}
+
+@Logger
+class Person {
+  constructor(public name = "Steven") {
+    console.log("Create a person object");
+  }
+}
+
+const person = new Person();
