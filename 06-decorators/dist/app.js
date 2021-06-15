45502cab5b44dce3ec82e383e762ebbad337f3aa
budokans <68995729+budokans@users.noreply.github.com>
2021-06-15 15:01:19 +1200

    106. Working with Decorator Factories

--- 06-decorators/dist/app.js
+++ 06-decorators/dist/app.js
@@ -5,9 +5,11 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log("Logging");
-    console.log(constructor);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
 let Person = class Person {
     constructor(name = "Steven") {
@@ -16,6 +18,6 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    Logger("Logging: person")
 ], Person);
 const person = new Person();
--- 06-decorators/src/app.ts
+++ 06-decorators/src/app.ts
@@ -1,11 +1,20 @@
 // Note that decorators execute when classes are defined; not when instances of said classes are instantiated.
 
-function Logger(constructor: Function) {
-  console.log("Logging");
-  console.log(constructor);
+// Decorator function
+// function Logger(constructor: Function) {
+//   console.log("Logging");
+//   console.log(constructor);
+// }
+
+// Decorator Factory - functions that take arguments that can be accessed by the returned decorator function.
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+@Logger("Logging: person")
 class Person {
   constructor(public name = "Steven") {
     console.log("Create a person object");
