efc948627ad92f269693088f4df23cd9f6703e9f
budokans <68995729+budokans@users.noreply.github.com>
2021-06-16 21:59:27 +1200

    Render form with OOP

--- 07-project-manager/dist/app.js
+++ 07-project-manager/dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log("Test");
+class Form {
+    constructor() {
+        this.templateElement = document.getElementById("project-input");
+        this.targetElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.formElement = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.targetElement.insertAdjacentElement("afterbegin", this.formElement);
+    }
+}
+const form = new Form();
--- 07-project-manager/src/app.ts
+++ 07-project-manager/src/app.ts
@@ -1,1 +1,28 @@
-console.log("Test");
+// Create a Form class that will get the content in the template and render it to the DOM on instantiation.
+
+class Form {
+  private templateElement: HTMLTemplateElement;
+  private targetElement: HTMLDivElement;
+  private formElement: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    ) as HTMLTemplateElement;
+    this.targetElement = document.getElementById("app") as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.formElement = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.targetElement.insertAdjacentElement("afterbegin", this.formElement);
+  }
+}
+
+const form = new Form();
