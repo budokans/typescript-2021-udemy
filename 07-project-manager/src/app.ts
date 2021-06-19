6065858bc598bce245f2ef6a6e38245a2020bf8f
budokans <68995729+budokans@users.noreply.github.com>
2021-06-19 19:02:09 +1200

    fix: string validation
    Validator accepts strings of length equal to minLength and strings of length equal to maxLength

--- 07-project-manager/src/app.ts
+++ 07-project-manager/src/app.ts
@@ -78,10 +78,10 @@ namespace App {
       isValid = isValid && inputValue.toString().trim().length > 0;
     }
     if (validatableInput.minLength != null && typeof inputValue === "string") {
-      isValid = isValid && inputValue.length > validatableInput.minLength;
+      isValid = isValid && inputValue.length >= validatableInput.minLength;
     }
     if (validatableInput.maxLength != null && typeof inputValue === "string") {
-      isValid = isValid && inputValue.length < validatableInput.maxLength;
+      isValid = isValid && inputValue.length <= validatableInput.maxLength;
     }
     if (validatableInput.min != null && typeof inputValue === "number") {
       isValid = isValid && inputValue >= validatableInput.min;
